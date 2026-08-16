bf60f0be0256a8e8b076c67b65aa40c679b570ed
Manuel Narciso <mnarcisom3@gmail.com>
2026-08-16 18:03:54 +0200

    Implement GWAS by chunks. Create function do_gwas and test it. Debug io_vcf.py from pynei. Add new dependencies

--- tests/time_testing/usar_pynei.py
+++ tests/time_testing/usar_pynei.py
@@ -4,21 +4,37 @@ from pathlib import Path
 
 from gwaslib import integration
 import pynei
+
 PROJECT_DIR = Path(__file__).parent.parent.parent
 
-path_VCF = PROJECT_DIR / "geno_pheno_files" / "vcf_test_247rows.vcf"
+# Los VCF para mis pruebas originales muy pequeños (muy pocos SNPs)
+# path_VCF = PROJECT_DIR / "geno_pheno_files" / "vcf_test_247rows.vcf"
 # path_VCF = PROJECT_DIR / "geno_pheno_files" / "vcf_7_samples.vcf"
-# 
 
-# Leemos el VCF con pynei
+# El de probar la web, de Varitome reducido a 100MB
+#path_VCF = PROJECT_DIR / "geno_pheno_files" / "VCF_FILES_from_Ximo" / "Varitome_reduced_all_chroms.vcf"
+
+# Una versión aún más reducida (20MB) para que el tiempo de computación sea menor
+path_VCF = PROJECT_DIR / "geno_pheno_files" / "VCF_FILES_from_Ximo" / "Varitome_20mb_reduced.vcf"
+
+# Una versión intermedia (50MB)
+#path_VCF = PROJECT_DIR / "geno_pheno_files" / "VCF_FILES_from_Ximo" / "Varitome_50mb_reduced.vcf"
+
+
+# Leemos el VCF con pynei y sacamos la mat012 sin filtrar nada
 variants = pynei.io_vcf.vars_from_vcf(path_VCF)
-matriz012_sin = pynei.pca.create_012_gt_matrix(variants, transform_to_biallelic=True)
-print(matriz012_sin.shape)
 
+matriz012_crude = pynei.pca.create_012_gt_matrix(variants, transform_to_biallelic=True)
+print(matriz012_crude.shape)
+# Para el vcf de 20MB   --> (4122, 143)
+# Para el vcf de 50MB   --> (69707, 166)
+# Para el vcf de 100MB  --> (27807, 166)
+
+# APRENDIENDO SOBRE EL FUNCIONAMIENTO DE PYNEI
 '''
 # DUDA 1 = Vamos a comprobar si un objeto Variants (tal cual del VCF) es un iterador de un solo uso
-matriz012_sin_2 = pynei.pca.create_012_gt_matrix(variants, transform_to_biallelic=True)
-print(matriz012_sin_2.shape)
+matriz012_crude_2 = pynei.pca.create_012_gt_matrix(variants, transform_to_biallelic=True)
+print(matriz012_crude_2.shape)
 
 # Ahora comprobémoslo para un objeto Variants FILTRADO
 filtered_variants = pynei.var_filters.filter_by_ld_and_maf(variants)
@@ -53,6 +69,8 @@ print(mat_b.shape)
 
 # Efectivamente, sí que podemos
 '''
+
+''' POR TIEMPO
 # DUDA 3 = ¿¿Obtener las samples_idx de un Variants filtrado consumirá el iterador??
 # Probemos
 filt = pynei.var_filters.filter_by_ld_and_maf(variants)
@@ -61,8 +79,10 @@ print(a)
 b = filt.samples
 print(b)
 # No lo consume
+'''
 
 
+# LO DE ESTE COMENTARIO ES DE LA PRIMERA VEZ QUE USÉ PYNEI (ignorarlo)
 '''
 # Hagamos el filtrado para el PCA (filtrar por maf y LD)
 filtered_variants = pynei.var_filters.filter_by_ld_and_maf(variants)
@@ -91,38 +111,9 @@ print(comb_lineales.shape)
 '''
 #---------------------------------------------------------------------------------------------------
 # Probemos las funciones de integration.py
-'''
-variants_2 = pynei.io_vcf.vars_from_vcf(path_VCF)
-filtered_vars_for_PCA = integration.filter_genotypes_for_PCA(variants_2)
-'''
-'''
-for chunk in filtered_vars_2.iter_vars_chunks():
-    print(chunk.alleles.head())
-    break
-'''
-'''
-matriz012_filt_2 = pynei.pca.create_012_gt_matrix(filtered_vars_for_PCA, transform_to_biallelic=True)
-print(matriz012_filt_2.shape)
+#---------------------------------------------------------------------------------------------------
 
-sample_ids = filtered_vars_for_PCA.samples
-print(sample_ids.shape)
-
-
-variants_3 = pynei.io_vcf.vars_from_vcf(path_VCF)
-filtered_vars_for_GWAS = integration.filter_genotypes_for_GWAS(variants_3)
-'''
-'''
-matriz012_filt_3 = pynei.pca.create_012_gt_matrix(filtered_vars_for_GWAS, transform_to_biallelic=True)
-print(matriz012_filt_3.shape)
-'''
-# ¿Converge el PCA si solo filtramos por MAF y no por LD? EN ESTE CASO SÍ
-
-'''
-# Sigamos probando funciones de integration.py
-all_info = integration._extract_variant_data(filtered_vars_for_GWAS)
-print(all_info.keys())
-'''
-
+# ---------------------------------------------------------------------------------------------------------------------------------------------
 # PROBEMOS LA CARGA DE FENOTIPOS
 excel_path_feno_quanti = PROJECT_DIR / "geno_pheno_files" / "PHENOTYPES_from_Ximo" / "Excel_files" / "quanti_trait_mean_fruit_weight.xlsx"
 csv_path_feno_quanti = PROJECT_DIR / "geno_pheno_files" / "PHENOTYPES_from_Ximo" / "CSV_files" / "quanti_trait_mean_fruit_weight.csv"
@@ -166,3 +157,67 @@ print(samples_known_pheno_quanti.shape)
 samples_known_pheno_quali = integration._get_samples_with_known_phenotypes(crude_feno_quali_1)
 print(samples_known_pheno_quali.shape)
 # Funciona correctamente
+
+# Probemos brevemente la función compare_sample_ids
+crude_id_comparison = integration.compare_crude_sample_ids(variants, crude_feno_quali_1)
+
+print(
+    f"{len(crude_id_comparison['shared'])} samples shared; "
+    f"{len(crude_id_comparison['genotype_only'])} genotype-only; "
+    f"{len(crude_id_comparison['phenotype_only'])} phenotype-only."
+)
+
+# ---------------------------------------------------------------------------------------------------------------------
+# PROBEMOS AHORA EL FILTRADO DE DATOS GENOTÍPICOS
+
+''''''
+# Filtrado para el PCA y comprobamos cuántos SNPs e individuos quedan
+filtered_vars_for_PCA = integration.filter_genotypes_for_PCA(variants, crude_feno_quali_2)
+
+# matriz012_filt_PCA = pynei.pca.create_012_gt_matrix(filtered_vars_for_PCA, transform_to_biallelic=True)
+# print(matriz012_filt_PCA.shape)
+'''
+#-------------------------------------------------------------------------------------
+# Para el vcf de 20MB   --> (4122, 143)
+# Para el vcf de 50MB   --> (8461, 143)
+# Para el vcf de 100MB  --> (17334, 143)
+#-------------------------------------------------------------------------------------
+'''
+sample_ids = filtered_vars_for_PCA.samples
+# print(sample_ids)
+''''''
+# Filtrado para el GWAS y comprobamos cuántos SNPs e individuos quedan
+filtered_vars_for_GWAS = integration.filter_genotypes_for_GWAS(variants, crude_feno_quali_2)
+
+# matriz012_filt_GWAS = pynei.pca.create_012_gt_matrix(filtered_vars_for_GWAS, transform_to_biallelic=True)
+# print(matriz012_filt_GWAS.shape)
+'''
+#-------------------------------------------------------------------------------------
+# Para el vcf de 20MB   --> (23504, 143)
+# Para el vcf de 50MB   --> (58979, 143)
+# Para el vcf de 100MB  --> (117912, 143)
+#-------------------------------------------------------------------------------------
+'''
+# print(filtered_vars_for_GWAS.samples)
+''''''
+# ¿Converge el PCA si solo filtramos por MAF y no por LD? EN ESTE CASO SÍ
+
+#--------------------------------------------------------------------------------------------
+# PROBEMOS LA FUNCIÓN do_gwas
+
+# Primero, filtramos el pd.Series de fenotipos para que los individuos coincidan con el variants
+filtered_phenotypes = integration.filter_phenotypes(crude_feno_quali_2, filtered_vars_for_GWAS.samples)
+print(filtered_phenotypes.to_numpy().shape)
+
+# Segundo, hacemos el PCA con Pynei, a partir del Variants filtrado
+PCA = pynei.pca.do_pca_with_vars(filtered_vars_for_PCA, transform_to_biallelic=True)
+
+# Ya tenemos todo
+gwas_results = integration.do_gwas(filtered_vars_for_GWAS,
+                filtered_phenotypes,
+                PCA["projections"],
+                type_of_phenotype="cualitativo (binario)",
+                sort_by_significance=True
+                )
+
+print(gwas_results)
